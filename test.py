70c543774d3879372c80c4e8772a801c50593c47
Alli Nilles <anilles137@gmail.com>
2019-12-02 16:11:10 -0600

    refactor into class

--- test.py
+++ test.py
@@ -9,50 +9,85 @@ sys.path.append('../bounce_viz/src/')
 from simple_polygon import Simple_Polygon
 from maps import small_square
 
-environment = Simple_Polygon("env", small_square[0], small_square[1:])
-
 TWOPI = 2*np.pi
+EPSILON = 0.0001
 
-fig, ax = plt.subplots()
+class Game(object):
 
-t = np.arange(0.0, TWOPI, 0.001)
-initial_amp = .5
-s = initial_amp*np.sin(t)
-l, = plt.plot(t, s, lw=2)
+    def __init__(self, environment):
 
-axis = plt.axis([0,TWOPI,-1,1])
+        self.environment = environment
+        self.fig, self.ax = plt.subplots()
 
-axamp = plt.axes([0.25, .03, 0.50, 0.02])
-# Slider
-samp = Slider(axamp, 'Amp', 0, 1, valinit=initial_amp)
+        self.t = np.arange(0.0, TWOPI, 0.001)
+        initial_amp = .5
+        s = initial_amp*np.sin(self.t)
+        self.l, = plt.plot(self.t, s, lw=2)
+
+        axis = plt.axis([0,TWOPI,-1,1])
+
+        axamp = plt.axes([0.25, .03, 0.50, 0.02])
+        # Slider
+        self.samp = Slider(axamp, 'Amp', 0, 1, valinit=initial_amp)
 
 
-def draw_poly(poly):
-    patches = []
+    def draw_poly(self, poly):
+        patches = []
 
-    outer = [p for (i,p) in poly.vertex_list_per_poly[0]]
-    p = Polygon(outer, ec='k', lw=2, fc='none')
-    patches.append(ax.add_patch(p))
+        outer = [p for (i,p) in poly.vertex_list_per_poly[0]]
+        p = Polygon(outer, ec='k', lw=2, fc='none')
+        patches.append(self.ax.add_patch(p))
 
-    for hole in poly.vertex_list_per_poly[1:]:
-        pass
+        # TODO implement holes
+        for hole in poly.vertex_list_per_poly[1:]:
+            pass
 
-    return patches
+        return patches
 
-def draw_trajectory(bounce_rule, start, n):
-    return
+    def draw_trajectory(self, start, k):
+        outer_map, holes_map = self.environment.compute_unit_interval_mapping()
+        pts = env.complete_vertex_list
+        n = env.size
+        if start < 0.0 or start > 1.0:
+            raise ValueError("Start position must be on polygon")
+        else:
+            #print(outer_map)
+            startv = outer_map[0]
+            i = 0
+            while startv < start:
+                i += 1
+                startv = outer_map[i]
+            print(pts[(i-1)%n])
+            print((start-outer_map[(i-1)%n]))
+            print(pts[i%n]-pts[(i-1)%n])
+            start = pts[(i-1)%n]+(start-outer_map[(i-1)%n])*(pts[i]-pts[(i-1)%n])
+            print(pts)
+            print(start)
 
-def update(val):
-    global environment
-    # amp is the current value of the slider
-    amp = samp.val
-    # update curve
-    l.set_ydata(amp*np.sin(t))
-    polys = draw_poly(environment)
-    # redraw canvas while idle
-    fig.canvas.draw_idle()
+        return
 
-# call update function on slider value change
-samp.on_changed(update)
+    def update(self, val):
+        # amp is the current value of the slider
+        amp = self.samp.val
+        # update curve
+        self.l.set_ydata(amp*np.sin(self.t))
+        polys = self.draw_poly(self.environment)
+        # redraw canvas while idle
+        self.fig.canvas.draw_idle()
 
-plt.show()
+
+    def run(self):
+        # call update function on slider value change
+        self.samp.on_changed(self.update)
+
+        plt.show()
+
+if __name__ == '__main__':
+
+
+    env = Simple_Polygon("env", small_square[0], small_square[1:])
+
+    g = Game(env)
+    g.draw_trajectory(0.5, 5)
+    #g.run()
+
